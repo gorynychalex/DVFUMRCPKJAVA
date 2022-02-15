db0d8853c4b04409092fd753b3245fe969cdc3da
gorynychalex <gorynychalex@gmail.com>
2022-02-15 19:49:25 +1000

    Start multithreads

---
+++ src/ru/dvfu/mrcpk/java03/example01/threads/AccountApp.java
@@ -0,0 +1,43 @@
+package ru.dvfu.mrcpk.java03.example01.threads;
+
+//Класс для тестовой демонстрации системы расчета - инкремент-декремент
+//Без потокозащищенности
+
+public class AccountApp {
+    public static void main(String[] args) {
+        Account account = new Account(100_000);
+        Thread withdrawThread = new AccountApp.WithDrawThread(account);
+        Thread depositThread = new AccountApp.DepositThread(account);
+        withdrawThread.start();
+        depositThread.start();
+        System.out.println("Stop balance = " + account.getBalance());
+    }
+    private static class WithDrawThread extends Thread {
+        private final Account account;
+
+        private WithDrawThread(Account account) {
+            this.account = account;
+        }
+
+        public void run() {
+            for (int i = 0; i < 20_000; i++) {
+                account.withdraw(1);
+            }
+        }
+    }
+
+    private static class DepositThread extends Thread {
+        private final Account account;
+
+        private DepositThread(Account account) {
+            this.account = account;
+        }
+
+        public void run() {
+            for (int i = 0; i < 20_000; i++) {
+                account.deposit(1);
+            }
+        }
+
+    }
+}
